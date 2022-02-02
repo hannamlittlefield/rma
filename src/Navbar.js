1689712ff9340e620d58898c81c11df50b29fb8c
Hanna <hannamlittlefield@gmail.com>
2022-02-02 10:38:23 -0500

    adding in files

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+export default function NavBar(){
+    return (
+
+<nav class="navbar navbar-expand-lg">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Schedule</a>
+      <a class="nav-item nav-link" href="#">FAQ</a>
+      <a class="nav-item nav-link" href="#">Training Videos</a>
+    </div>
+  </div>
+</nav>
+    )
+}
